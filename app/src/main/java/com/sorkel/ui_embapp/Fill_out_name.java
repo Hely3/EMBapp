944f86d63416217f5f511b2cc27a0db8a1637356
Hely3 <48092042+Hely3@users.noreply.github.com>
2019-02-28 13:48:25 +0100

    Initial commit

---
+++ app/src/main/java/com/sorkel/ui_embapp/Fill_out_name.java
@@ -0,0 +1,13 @@
+package com.sorkel.ui_embapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Fill_out_name extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fill_out_name);
+    }
+}
---
+++ app/src/main/java/com/sorkel/ui_embapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sorkel.ui_embapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void close_app (View view){
+        closeContextMenu();
+    }
+}
